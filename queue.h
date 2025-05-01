e1822affd898cbf9f7756f7873d0bc3488f31bfc
MooDengHippo <jadet26368@gmail.com>
2025-05-01 20:27:02 +0700

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,6 +1,7 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 #include "graph.h"
+
 // Queue node for holding GraphNode pointers
 typedef struct QueueNode{
 
@@ -16,19 +17,14 @@ typedef struct Queue{
     QueueNode *rear;
     
 }Queue;
-
 // Initialize a new empty queue
 Queue *queue_init(void);
-
 // Add a node to the rear of the queue
 void enqueue(Queue *queue, GraphNode *node);
-
 // Remove and return the front node of the queue
 GraphNode *dequeue(Queue *queue);
-
 // Check if the queue is empty
 int queue_is_empty(Queue *queue);
-
 // Free all memory associated with the queue
 void queue_free(Queue *queue);
 
