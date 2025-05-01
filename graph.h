274dfae5c7dec8aa67c28c08f7b4b4cd57e0cf13
MooDengHippo <jadet26368@gmail.com>
2025-05-01 20:19:33 +0700

    Update graph.h

--- graph.h
+++ graph.h
@@ -5,7 +5,7 @@
 #define MAX_NEIGHBORS 10 // Limit of neighbors per node
 /*
  * GraphNode
- * ----------------------------------
+ * ----------------------------------------------------
  * Represents a phone number and its direct connections.
  */
 typedef struct GraphNode{
@@ -16,19 +16,14 @@ typedef struct GraphNode{
     int visited;                                // BFS/DFS visited flag
 
 }GraphNode;
-
 // Get or create a graph node for a phone number.
 GraphNode *graph_get_node(GraphNode *nodes[], const char *phone);
-
 // Add a bidirectional edge between two phone numbers.
 void graph_add_edge(GraphNode *nodes[], const char *p1, const char *p2);
-
 // Perform BFS traversal from a starting phone number.
 void graph_bfs(GraphNode *nodes[], const char *start_phone);
-
 // Perform DFS traversal from a starting phone number.
 void graph_dfs(GraphNode *nodes[], const char *start_phone);
-
 // Free a graph node and clear its data.
 void graph_node_free(GraphNode *node);
 
